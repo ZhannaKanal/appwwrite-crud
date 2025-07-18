e70a3cc82fc85282117706268221bd774f25d1fb
Zhanna <kezhanna.03@mail.ru>
2025-07-18 12:44:54 +0500

    fixed

--- src/app/api/interpretations/[id]/route.ts
+++ src/app/api/interpretations/[id]/route.ts
@@ -1,7 +1,6 @@
 import client from "@/lib/appwrite_client";
 import { Databases } from "appwrite";
 import { NextResponse } from "next/server";
-import { use } from "react";
 
 const database = new Databases(client);
 
@@ -42,7 +41,7 @@ async function updateInterpretation(
     const response = await database.updateDocument(
       "6878ebb60016140f52c1",
       "6878ec2a00166e4ee89c",
-      id,
+        id,
       data
     );
   } catch (error) {
@@ -50,62 +49,42 @@ async function updateInterpretation(
     throw new Error("Error to update interpretation");
   }
 }
-
 export async function GET(
   req: Request,
-  { params }: { params: Promise<{ id: string }> }
+  { params }: { params: { id: string } }
 ) {
   try {
-    const { id } = use(params);
+    const { id } = params;
     const interpretation = await fetchInterpretation(id);
     return NextResponse.json({ interpretation });
   } catch (error) {
-    return NextResponse.json(
-      {
-        error: "Failed to interpretation",
-      },
-      {
-        status: 500,
-      }
-    );
+    return NextResponse.json({ error: "Failed to fetch interpretation" }, { status: 500 });
   }
 }
+
 export async function DELETE(
   req: Request,
-  { params }: { params: Promise<{ id: string }> }
+  { params }: { params: { id: string } }
 ) {
   try {
-    const { id } = use(params);
+    const { id } = params;
     await deleteInterpretation(id);
     return NextResponse.json({ message: "Interpretation deleted" });
   } catch (error) {
-    return NextResponse.json(
-      {
-        error: "Failed to delete interpretation",
-      },
-      {
-        status: 500,
-      }
-    );
+    return NextResponse.json({ error: "Failed to delete interpretation" }, { status: 500 });
   }
 }
+
 export async function PUT(
   req: Request,
-  { params }: { params: Promise<{ id: string }> }
+  { params }: { params: { id: string } }
 ) {
   try {
-    const { id } = use(params);
+    const { id } = params;
     const interpretation = await req.json();
     await updateInterpretation(id, interpretation);
     return NextResponse.json({ message: "Interpretation updated" });
   } catch (error) {
-    return NextResponse.json(
-      {
-        error: "Failed to update interpretation",
-      },
-      {
-        status: 500,
-      }
-    );
+    return NextResponse.json({ error: "Failed to update interpretation" }, { status: 500 });
   }
 }
